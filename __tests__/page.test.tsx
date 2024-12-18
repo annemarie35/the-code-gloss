75ce22b66e4bd8008471bc2138b5a11c4e74ba9e
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-18 16:02:56 +0100

    add first simple about page

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,16 +1,29 @@
-import { expect, it, describe } from "vitest";
+import { expect, it, describe, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import React from "react";
 import Page from "@/app/page";
 
 describe("Page", () => {
   render(<Page />);
-  expect(
-    screen.getByRole("heading", { level: 1, name: "The Code Gloss" }),
-  ).toBeDefined();
+  it("should contain a title", () => {
+    expect(
+      screen.getByRole("heading", { level: 1, name: "The Code Gloss" }),
+    ).toBeDefined();
+  });
 
   it("should contain a form", async () => {
-    const wrapper = render(<Page />);
-    expect(await wrapper.getAllByTestId("gloss-terms-form")).toBeDefined();
+    expect(screen.getAllByTestId("gloss-terms-form")).toBeDefined();
+  });
+
+  it("Click the about router link", async () => {
+    const user = userEvent.setup();
+    const about = vi.spyOn(user, "click");
+    const aboutLink = screen.getByText(/About/i);
+
+    await user.click(aboutLink);
+
+    expect(about).toHaveBeenCalledTimes(1);
   });
 });
--- __tests__/pages/about.test.tsx
+++ __tests__/pages/about.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import About from "@/src/routes/about";
+import About from "@/pages/about";
 
 describe("About", () => {
   it("should render About page", () => {
