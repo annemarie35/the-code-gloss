3b9299ef7fc493e81ec9515d2bd77d38a97609f1
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-23 16:29:31 +0100

    add a footer with link component

---
+++ __tests__/src/components/footer.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import Footer from "@/src/components/footer";
+
+describe("Footer", () => {
+  it("renders a footer", async () => {
+    const { getByRole } = render(<Footer />);
+    expect(getByRole("link")).toHaveAttribute("href", "/about");
+  });
+});
