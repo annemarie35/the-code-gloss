23547cab345303be5e2d5322c7cf359be2911eb6
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-23 14:27:52 +0100

    add some parametrized font size to tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xs: ["12px", "20px"],
+        sm: ["14px", "22px"],
+        base: ["16px", "24px"],
+        medium: ["20px", "30px"],
+        lg: ["24px", "38px"],
+        xl: ["30px", "44px"],
+        "2xl": ["36px", "54px"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        neutral: {
+          50: "#fffefa",
+        },
       },
     },
   },
