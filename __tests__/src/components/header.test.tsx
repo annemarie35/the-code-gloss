da258fe9b49382fa2c5597d8e125d26399186ce7
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-23 14:35:58 +0100

    add a simple header component

---
+++ __tests__/src/components/header.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Header from "@/src/components/header";
+
+describe("Header", () => {
+  it("renders an header with title", async () => {
+    render(<Header title="Some title" />);
+    expect(await screen.findByText("Some title")).toBeDefined();
+  });
+});
