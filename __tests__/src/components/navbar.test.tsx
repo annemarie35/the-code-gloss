e5db7ab11965397390aa5a20e61ddd6ced3b6204
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-24 13:44:59 +0100

    test link without clicking and remove jsom error

--- __tests__/src/components/navbar.test.tsx
+++ __tests__/src/components/navbar.test.tsx
@@ -1,18 +1,10 @@
-import { describe, expect, it, vi } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { render } from '@testing-library/react'
 import Navbar from '@/src/components/navbar'
-import userEvent from '@testing-library/user-event'
 
 describe('Navbar', () => {
-    it('Click the about router link', async () => {
-        render(<Navbar />)
-
-        const user = userEvent.setup()
-        const about = vi.spyOn(user, 'click')
-        const aboutLink = screen.getByText(/About/i)
-
-        await user.click(aboutLink)
-
-        expect(about).toHaveBeenCalledTimes(1)
+    it('should have a link to about page', async () => {
+        const { getByRole } = render(<Navbar />)
+        expect(getByRole('link', { name: 'About' })).toHaveAttribute('href', '/about')
     })
 })
