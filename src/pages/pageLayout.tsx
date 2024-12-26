8797695d2b57041e2c1ad3528ba3443bf8fd2a34
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-26 14:16:14 +0100

    (style) : make page full height

--- src/pages/pageLayout.tsx
+++ src/pages/pageLayout.tsx
@@ -8,14 +8,14 @@ type MyComponentProps = React.PropsWithChildren<{ title?: string }>
 export default function PageLayout({ children, ...props }: MyComponentProps) {
     const { title } = props
     return (
-        <div className="bg-[#FFFDEA] px-10 h-screen">
+        <div className="bg-[#FFFDEA] px-10 h-screen flex flex-col">
             <div className="pb-4">
                 <p className="text-[#EB6393] text-4xl ml-3">The</p>
                 <p className="text-[#C4338E] text-4xl ml-10">Code</p>
                 <p className="text-[#D53815] text-4xl">Gloss</p>
             </div>
             <Header title={title ?? ''} />
-            <div className="py-8">{children}</div>
+            <div className="py-8 px-2 h-full">{children}</div>
             <Footer />
         </div>
     )
