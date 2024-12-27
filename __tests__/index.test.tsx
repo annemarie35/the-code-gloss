c354c7195037883baadd3e8f0b1cac194b67130a
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-27 10:47:04 +0100

    refacto : move files in app folder to src/pages folder

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,7 +2,7 @@ import { expect, it, describe } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import React from 'react'
-import Page from '@/app/page'
+import Page from '@/src/pages'
 
 describe('Page', () => {
     render(<Page />)
