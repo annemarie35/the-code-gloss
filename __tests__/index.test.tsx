de12613700fe8cfc327f75a1b1c9f4bfe4ad5c4f
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2024-12-30 13:36:04 +0100

    get gloses on loading home page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,17 @@
-import { expect, it, describe } from 'vitest'
+import { expect, it, describe, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import React from 'react'
 import Page from '@/src/pages'
 
 describe('Page', () => {
+    global.fetch = vi.fn()
+
+    function createFetchResponse(data: []) {
+        return { json: () => new Promise((resolve) => resolve(data)) }
+    }
+    vi.mock('fetch', () => vi.fn().mockResolvedValue(createFetchResponse([])))
+
     render(<Page />)
     it('should contain a title', () => {
         expect(screen.getByRole('heading', { level: 2, name: '✨ Make your code base shine ✨' })).toBeDefined()
