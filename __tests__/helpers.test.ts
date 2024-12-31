d9857522b0d8b3b72f1f632714614c45cb14f9e9
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2024-12-31 12:08:05 +0100

    add test helper to mock api call with success or failure

---
+++ __tests__/helpers.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest'
+import { Glose } from '@/src/lib/get-gloses'
+import { createFetchResponse } from '@/__tests__/helpers'
+
+describe('Test helpers', () => {
+    describe('createFetchResponse', () => {
+        it('should mock a fetch response with data', async () => {
+            const gloses: Glose[] = [
+                {
+                    created_at: 'date',
+                    description: 'Invented by Alistair Cockburn in 2005',
+                    id: 8,
+                    tags: 'Craft, Architecture',
+                    title: 'Hexagonale architecture'
+                }
+            ]
+
+            const response = createFetchResponse({ data: gloses, ok: true })
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            // TODO Fixme
+            expect(await response.json()).toEqual(gloses)
+            expect(response.ok).toEqual(true)
+            expect(response.status).toEqual(200)
+        })
+
+        it('should mock a fetch response in error', async () => {
+            const response = createFetchResponse({ ok: false })
+            expect(response).toEqual({ json: undefined, ok: false, status: 500 })
+        })
+    })
+})
