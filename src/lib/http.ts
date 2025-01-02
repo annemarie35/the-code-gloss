6b313d3efffe8d36cffe284b7df4b264812d4218
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-02 15:54:11 +0100

    refacto : catch errors in httpClient

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -19,9 +19,11 @@ export const httpClient = async ({ url, requestConfig }: HttpClient) => {
         if (response.ok) {
             return await response.json()
         } else {
+            throw new Error('Erreur dans récupérations des gloses')
             // TODO something to manage properly errors code https://github.com/annemarie35/the-code-gloss/issues/10
         }
     } catch (error) {
+        throw new Error(' encore une erreur dans le catch de http client')
         logger.error(error)
     }
 }
