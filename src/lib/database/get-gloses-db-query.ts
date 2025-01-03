3fbd8b7ac2ab8a1a156cb04f5f0c7e064c13793c
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-03 09:00:13 +0100

    wip

--- src/lib/database/get-gloses-db-query.ts
+++ src/lib/database/get-gloses-db-query.ts
@@ -1,5 +1,7 @@
 'use server'
 import { databaseClient } from '@/src/lib/database/database-client'
+import knex from 'knex'
+import { dbConfig } from '@/src/lib/database/database-config'
 
 export type Glose = {
     id: number
@@ -10,10 +12,16 @@ export type Glose = {
 }
 export const getGlosesDbQuery = async (): Promise<Glose[]> => {
     try {
+        const { rows } = await knex(dbConfig)
+            .raw('select * from gloses')
+            .then((resp) => resp)
+
+        console.log(rows)
         const gloses = await databaseClient.select('*')
-        return gloses
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        // act like if last knex query is still in memory so it is not a select that is done when page is refresh, it create some more entries
+        return rows
     } catch (error) {
+        console.error('>>>>>>>>>>>>>>>>>>>>>', error)
         throw new Error('Database error')
     }
 }
