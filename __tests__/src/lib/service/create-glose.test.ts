60630626da33da37bbf708c7ea98842b8d45f2ac
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-03 17:22:46 +0100

    :wastebasket: refacto : remove console logs

--- __tests__/src/lib/service/create-glose.test.ts
+++ __tests__/src/lib/service/create-glose.test.ts
@@ -5,18 +5,7 @@ import { createGlose } from '@/src/lib/service/create-glose'
 
 describe('createGlose', () => {
     it('Should persist glose', async () => {
-        console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)))
-        console.log(new Date(2006, 0, 2, 15, 4, 5).toUTCString())
-        console.log(
-            JSON.stringify(
-                new Date(
-                    Date.parse(new Date(2006, 0, 2, 15, 4, 5).toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }))
-                )
-            )
-        )
-
         vi.useFakeTimers()
-        // const date = new Date('1995-12-17T03:24:00)').toUTCString()
         const date = new Date(
             Date.parse(new Date(2006, 0, 2, 15, 4, 5).toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }))
         )
