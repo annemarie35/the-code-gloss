afb12fb56d75b9d8b740dde72438295087d8ad35
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-04-24 08:43:18 +0200

    refacto : use direnv in pg connection pool configuration

--- infra/database/connectionPool.ts
+++ infra/database/connectionPool.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.POSTGRESQL_HOST,
+    port: Number(process.env.POSTGRESQL_PORT),
+    database: process.env.POSTGRESQL_DATABASE,
+    user: process.env.POSTGRESQL_USERNAME,
+    password: process.env.POSTGRESQL_PASSWORD,
     application_name: 'code-gloss',
     max: 6,
     idleTimeoutMillis: 0,
@@ -15,3 +15,4 @@ const pool = new Pool({
 
 export const query = (queryTextOrConfig: string, values?: never | string[]) => pool.query(queryTextOrConfig, values)
 export const end = async () => await pool.end()
+// TODO use end when app is shut down
