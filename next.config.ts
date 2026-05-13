7aea673c689552f69db2e5360f5a05303679b8e7
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2026-05-13 19:28:39 +0200

    Fix: Add CSP in your server or framework configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from 'next'
 
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' data:;
+    font-src 'self';
+    connect-src 'self';
+    frame-ancestors 'none';
+    base-uri 'self';
+    form-action 'self';
+`
+    .replace(/\s{2,}/g, ' ')
+    .trim()
+
 const nextConfig: NextConfig = {
-    poweredByHeader: false
+    poweredByHeader: false,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 export default nextConfig
