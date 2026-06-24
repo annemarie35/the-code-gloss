b14ace3d82fc38698e0eb4cd83fb288cbcecc8d5
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2026-06-24 09:04:25 +0200

    feat(delete people): delete people repo

--- infra/repositories/person_repository.ts
+++ infra/repositories/person_repository.ts
@@ -1,5 +1,5 @@
 import { query } from '@/infra/database/connectionPool.ts'
-import { INSERT_PERSON, SELECT_ALL_PEOPLE } from '@/infra/database/sql_queries.ts'
+import { INSERT_PERSON, SELECT_ALL_PEOPLE, DELETE_PERSON } from '@/infra/database/sql_queries.ts'
 import { Person } from '@/src/core/domain/Types/Person.ts'
 import type { QueryResultRow } from 'pg'
 
@@ -20,4 +20,8 @@ export const insertPerson = async (person: Omit<Person, 'id'>): Promise<void> =>
 export const selectAllPeople = async (): Promise<QueryResultRow[]> => {
     const { rows } = await query(SELECT_ALL_PEOPLE('public'), [])
     return rows
+}
+
+export const deletePerson = async (id: number): Promise<void> => {
+    await query(DELETE_PERSON('public'), [id])
 }
